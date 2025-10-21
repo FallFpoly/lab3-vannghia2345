19f8bd964cc9299b1ed13154a39ab5aad252eac8
Admin <Admin@chotaoxin500k>
2025-10-21 10:24:53 +0700

    lap1

--- Lab3/src/main/java/com/mycompany/lab3/bai1.java
+++ Lab3/src/main/java/com/mycompany/lab3/bai1.java
@@ -11,6 +11,6 @@ package com.mycompany.lab3;
 public class bai1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello the gioi!");
     }
 }
